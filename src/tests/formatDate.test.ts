1cfdcf292c85a3ea9716e83720f62660113575dd
LiyanNguyen <liyannguyenn@gmail.com>
2023-07-15 20:45:34 +0800

    Update formatDate.test.ts

--- src/tests/formatDate.test.ts
+++ src/tests/formatDate.test.ts
@@ -2,9 +2,16 @@ import { describe, it, expect } from 'vitest'
 import formatDate from "../utils/formatDate"
 
 describe('Format date function', () => {
-  const newDate = new Date("2022-03-25");
-  it('Returns the date in dd-mm-yyyy format', () => {
-    const formattedDate = formatDate(newDate)
-    expect(formattedDate).toBe('25-03-2022')
+  const testDate1 = new Date("2022-03-05")
+  const testDate2 = new Date("2023-10-17")
+
+  it('returns date string in dd-mm-yyyy format adding leading 0s if possible', () => {
+    const formattedDate = formatDate(testDate1)
+    expect(formattedDate).toBe('05-03-2022')
+  })
+
+  it('returns date string in dd-mm-yyyy format', () => {
+    const formattedDate = formatDate(testDate2)
+    expect(formattedDate).toBe('17-10-2023')
   })
 })
