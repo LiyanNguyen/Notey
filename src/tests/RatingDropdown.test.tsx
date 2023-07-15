847404a747111b662f9a99c1c2af231859408e9e
LiyanNguyen <liyannguyenn@gmail.com>
2023-07-15 20:36:37 +0800

    Update RatingDropdown.test.tsx

--- src/tests/RatingDropdown.test.tsx
+++ src/tests/RatingDropdown.test.tsx
@@ -2,6 +2,7 @@ import { describe, it, expect } from 'vitest'
 import { render, screen } from '@testing-library/react'
 import { RatingDropdown } from '../components'
 import { RecoilRoot } from 'recoil'
+import userEvent from '@testing-library/user-event'
 
 describe('RatingDropdown', () => {
   it('renders correctly with the color options', () => {
@@ -11,6 +12,29 @@ describe('RatingDropdown', () => {
     const optionElements = screen.getAllByRole('option')
 
     expect(selectElement).toBeInTheDocument()
-    optionElements.forEach((item, index) => expect(item).toHaveValue(allRatingOptions[index]))
+    optionElements.forEach((item, index) => {
+      expect(item).toBeInTheDocument()
+      expect(item).toHaveValue(allRatingOptions[index])
+    })
+  })
+
+  it('changes sorting order to ascending', async () => {
+    render(<RatingDropdown />, { wrapper: RecoilRoot })
+    const selectElement = screen.getByRole('combobox')
+    const ascendingOption: HTMLOptionElement = screen.getByRole('option', { name: 'Ascending' })
+
+    await userEvent.selectOptions(selectElement, ascendingOption)
+
+    expect(ascendingOption.selected).toBe(true)
+  })
+
+  it('changes sorting order to descending', async () => {
+    render(<RatingDropdown />, { wrapper: RecoilRoot })
+    const selectElement = screen.getByRole('combobox')
+    const descendingOption: HTMLOptionElement = screen.getByRole('option', { name: 'Descending' })
+
+    await userEvent.selectOptions(selectElement, descendingOption)
+
+    expect(descendingOption.selected).toBe(true)
   })
 })
