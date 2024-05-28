d82d8f34b9709e3ea745e02ab482f69d9c549111
LiyanNguyen <liyannguyenn@gmail.com>
2024-05-29 06:25:10 +0900

    chore: package clean up

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,56 +1,9 @@
-import { faker } from "@faker-js/faker";
-import { Note } from "../types/Note";
-import { v4 as uuidv4 } from 'uuid';
-
-export const colorOptions = ['all','blue', 'red', 'yellow', 'green', 'slate']
-export const ratingOptions = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+export const colorOptions = ["all", "blue", "red", "yellow", "green", "slate"];
+export const ratingOptions = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 export const languages = [
   { name: "English", code: "en" },
   { name: "日本語", code: "jp" },
 ];
-
-export const fakeArray: Note[] = [
-  {
-    _id: uuidv4(),
-    title: faker.commerce.product(),
-    description: faker.commerce.productDescription(),
-    rating: 7,
-    color: 'blue',
-    createdAt: new Date(),
-  },
-  {
-    _id: uuidv4(),
-    title: faker.commerce.product(),
-    description: faker.commerce.productDescription(),
-    rating: 6,
-    color: 'red',
-    createdAt: new Date(),
-  },
-  {
-    _id: uuidv4(),
-    title: faker.commerce.product(),
-    description: faker.commerce.productDescription(),
-    rating: 8,
-    color: 'yellow',
-    createdAt: new Date(),
-  },
-  {
-    _id: uuidv4(),
-    title: faker.commerce.product(),
-    description: faker.commerce.productDescription(),
-    rating: 5,
-    color: 'green',
-    createdAt: new Date(),
-  },
-  {
-    _id: uuidv4(),
-    title: faker.commerce.product(),
-    description: faker.commerce.productDescription(),
-    rating: 4,
-    color: 'slate',
-    createdAt: new Date(),
-  },
-]
 
 export const colorClasses = {
   blue: { borderColor: "border-t-blue-400", ratingColor: "bg-blue-400" },
